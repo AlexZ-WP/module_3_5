b9e59393592940e5981e0857fbef28bfcb4f4834
AlexZ-WP <alexz10@mail.ru>
2024-10-20 20:36:06 +0300

    Add files via upload

--- module_3_5.py
+++ module_3_5.py
@@ -1,6 +1,6 @@
 def get_multiplied_digits(number):
     str_number = str(number)
-    str_number = get_multiplied_digits(str(number))
+    #str_number = get_multiplied_digits(str(number))
     first = int(str_number[0])
 
     if len(str_number) <= 1:
